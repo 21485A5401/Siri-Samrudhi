d72928fa5e6eae0402d9fa0071578ea8df6ab262
satya <21485A5401@gecgudlavallerumic.in>
2024-11-09 12:11:35 +0530

    added new apis

--- controllers/chartbot.js
+++ controllers/chartbot.js
@@ -7,7 +7,7 @@ const fs = require('fs').promises;
 const path = require('path');
 const mime = require("mime-types")
 const User = require("../models/User")
-dotenv.config()
+dotenv.config();
 
 
 const randomString = (length) => {
